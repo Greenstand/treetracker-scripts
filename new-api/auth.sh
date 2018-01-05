58e4372a229a59a748ad402bf1ad1521b0dddc54
Matthew Xi <matthew.fullstackgis@gmail.com>
2018-01-04 20:14:55 -0700

    Added auth and newuser scripts for new api

---
+++ new-api/auth.sh
@@ -0,0 +1,1 @@
+curl -v -X POST  -H "Content-Type: application/json"  -d @auth.json http://localhost:3000/auth/token
---
+++ new-api/newuser.sh
@@ -0,0 +1,1 @@
+curl -v -X POST  -H "Content-Type: application/json"  -d @newuser.json http://localhost:3000/auth/register
