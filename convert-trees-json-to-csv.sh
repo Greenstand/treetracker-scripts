b93594d98fc7d4af37b128f317b3102dda0c1c68
Mengchen Gao <gao.mengc@husky.neu.edu>
2020-07-25 18:56:04 -0400

    Updated code to added header in csv files and added comment in code.

--- convert-trees-json-to-csv.sh
+++ convert-trees-json-to-csv.sh
@@ -1,2 +1,5 @@
-jq -r '.trees[] | [.attribute, .device_identifier, .gps_accuracy, .image_url, .lat, .lon, .note, .planter_identifier, .sequence_id, .timestamp, .user_id, .uuid] | @csv '  
-
+# To output an CSV file use: 
+# ./convert-trees-json-to-csv.sh < input.json | > output.csv
+# To output csv raw data use:
+# ./convert-trees-json-to-csv.sh < input.json 
+jq -r '(.trees[0] | keys), (.trees[] | [.attribute, .device_identifier, .gps_accuracy, .image_url, .lat, .lon, .note, .planter_identifier, .sequence_id, .timestamp, .user_id, .uuid]) | @csv '  
